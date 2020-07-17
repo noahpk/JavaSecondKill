061e3160c205b595d0023b0692ef1241464d9b0c
Yang ShaoYong 杨少永 <shaoyong@sky-light.com.hk>
2020-07-17 14:42:22 +0800

    first code add

---
+++ docs/miaosha.sql
@@ -0,0 +1,118 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : aliyunTest
+Source Server Version : 50648
+Source Host           : 39.100.105.225:3306
+Source Database       : miaosha
+
+Target Server Type    : MYSQL
+Target Server Version : 50648
+File Encoding         : 65001
+
+Date: 2020-07-16 16:30:48
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '商品名称',
+  `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
+  `goods_img` varchar(640) DEFAULT NULL COMMENT '商品的图片',
+  `goods_detail` longtext COMMENT '商品的详情介绍',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1表示没有限制',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('1', 'iPhone', 'iPhone 11 Plus', 'https://img.pconline.com.cn/images/product/1048/1048848/z-5_sn8.jpg', '苹果最 新款手机', '5998.00', '50');
+
+-- ----------------------------
+-- Table structure for miaosha_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `miaosha_goods`;
+CREATE TABLE `miaosha_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '秒杀的商品表',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品Id',
+  `miaosha_price` decimal(10,2) DEFAULT '0.00' COMMENT '秒杀价',
+  `stock_count` int(11) DEFAULT NULL COMMENT '库存数量',
+  `start_date` datetime DEFAULT NULL COMMENT '秒杀开始时间',
+  `end_date` datetime DEFAULT NULL COMMENT '秒杀结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of miaosha_goods
+-- ----------------------------
+INSERT INTO `miaosha_goods` VALUES ('1', '1', '4998.00', '4', '2020-07-16 10:25:18', '2021-07-16 13:25:22');
+
+-- ----------------------------
+-- Table structure for miaosha_order
+-- ----------------------------
+DROP TABLE IF EXISTS `miaosha_order`;
+CREATE TABLE `miaosha_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_uid_gid` (`user_id`,`goods_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1552 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of miaosha_order
+-- ----------------------------
+INSERT INTO `miaosha_order` VALUES ('1551', '13309133912', '1565', '1');
+
+-- ----------------------------
+-- Table structure for miaosha_user
+-- ----------------------------
+DROP TABLE IF EXISTS `miaosha_user`;
+CREATE TABLE `miaosha_user` (
+  `id` bigint(20) NOT NULL COMMENT '用户ID，手机号码',
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(32) DEFAULT NULL COMMENT 'MD5(MD5(pass明文+固定salt) + salt)',
+  `salt` varchar(10) DEFAULT NULL,
+  `head` varchar(128) DEFAULT NULL COMMENT '头像，云存储的ID',
+  `register_date` datetime DEFAULT NULL COMMENT '注册时间',
+  `last_login_date` datetime DEFAULT NULL COMMENT '上蔟登录时间',
+  `login_count` int(11) DEFAULT '0' COMMENT '登录次数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of miaosha_user
+-- ----------------------------
+INSERT INTO `miaosha_user` VALUES ('13309133912', 'zhaosi', '469fae703f3f86c3c86b0837682ea687', 'mysaltab', null, null, null, '0');
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  `delivery_addr_id` bigint(20) DEFAULT NULL COMMENT '收获地址ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '冗余过来的商品名称',
+  `goods_count` int(11) DEFAULT '0' COMMENT '商品数量',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `order_channel` tinyint(4) DEFAULT '0' COMMENT '1pc，2android，3ios',
+  `status` tinyint(4) DEFAULT '0' COMMENT '订单状态，0新建未支付，1已支付，2已发货，3已收货，4已退款，5已完成',
+  `create_date` datetime DEFAULT NULL COMMENT '订单的创建时间',
+  `pay_date` datetime DEFAULT NULL COMMENT '支付时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1566 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of order_info
+-- ----------------------------
+INSERT INTO `order_info` VALUES ('1565', '13309133912', '1', null, 'iPhone', '1', '4998.00', '1', '0', '2020-07-16 10:39:53', null);
