6d4bfad87a8642953ae57b5546215ad605fc5fcb
noahpk <450070275@qq.com>
2020-07-21 10:06:06 +0800

    Revert "first code add"
    This reverts commit 061e3160

--- src/main/java/com/taotao/miaosha/redis/KeyPrefix.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.miaosha.redis;
-
-public interface KeyPrefix {
-		
-	public int expireSeconds();//返回过期时间
-	
-	public String getPrefix();//获取key
-	
-}
--- src/main/java/com/taotao/miaosha/redis/OrderKey.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.miaosha.redis;
-
-public class OrderKey extends BasePrefix {
-
-	public OrderKey(String prefix) {
-		super(prefix);
-	}
-	public static OrderKey getMiaoshaOrderByUidGid = new OrderKey("moug");
-}
